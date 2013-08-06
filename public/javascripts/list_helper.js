72c1aca09bafe140260feec2a135449f7b84d161
Hack Reactor Students <github@hackreactor.com>
2013-08-06 11:29:45 -0700

    list buttons are now highlighting properly
    -wip on loading proper page

--- public/javascripts/list_helper.js
+++ public/javascripts/list_helper.js
@@ -1,6 +1,6 @@
 
 var append_proxlist = function(numbertoappend){
-  $('#list').html('')
+  //$('#list').html('')
   for (var i = 0 ; i < numbertoappend; i++){
     $('#list').append("<li><a href='#'>"+ markers[i].title +'<br>'+'<span id="distance">'+markers[i].distancefromnav.toFixed() +' m'+'</span>'+"</a></li>")
   }
